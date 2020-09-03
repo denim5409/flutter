46678c468917c0773e56ed8627996d21b7b1f113
unknown <denim3@hanmail.net>
2020-09-03 20:10:07 +0900

    flutter added

---
+++ myapp/myapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BBANTO'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[Text('Hello'), Text('Hello'), Text('Hello')],
+          ),
+        ),
+      ),
+    );
+  }
+}
